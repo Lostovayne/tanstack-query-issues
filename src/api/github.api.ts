a1f5e182c468d397af6d6f593c99f143ce8096ff
LostoVayne <moises.barrera.dev@gmail.com>
2024-07-25 00:51:41 -0400

    agrega el token de github para las peticiones

--- src/api/github.api.ts
+++ src/api/github.api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-
-
 export const githubApi = axios.create({
-  baseURL:"https://api.github.com/repos/facebook/react",
-  headers:{
+  baseURL: "https://api.github.com/repos/facebook/react",
+  headers: {
     // TODO: Api Key de Github
-  }
-})
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+  },
+});
